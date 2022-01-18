0c9907cb7d49709e4fcbac60a9580f49a4d15b64
augie <augie@keatinge.com>
2022-01-17 17:00:13 -0800

    okish
    added hero image pls make it move

--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,27 @@
 
 public class Hero {
+	Hero Character;
+Hero(){
+	int CharX;
+	int CharY;
+	Character = new Hero();
+	
+}
+
+public void moveRight(int CharX,int CharY){
+	Character.
+}
+public void moveLeft(int CharX,int CharY){
+	
+}
+public void moveFoward(int CharX,int CharY){
+	
+}
+public void moveBackwards(int CharX,int CharY){
+	
+}
+public void Interact(int CharX,int CharY){
+	
+}
 
 }
